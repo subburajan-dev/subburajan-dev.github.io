7046e45ca38360984a1944713ca2f2924a6622bb
Subburajan <subburajanperumal@gmail.com>
2022-05-07 08:57:38 +0530

    feature/github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./component/Header";
-
+import Profile from "./component/Profile"
 function App() {
   return (
+    <div>
     <Header />
+    <Profile />
+    
+    </div>
   );
 }
 
---
+++ src/component/Profile.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+// import { Navbar, Nav, Container} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Profile extends Component{
+    render(){
+        return (
+            <p>profile</p>
+        )
+    }
+}
+export default Profile
